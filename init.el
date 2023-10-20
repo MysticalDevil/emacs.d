0640517cf18a4fe5372fec46a4436224491798a4
MysticalDevil <jinpeng7981@outlook.com>
2023-10-20 00:02:21 -0700

    feat: Add smooth-scrolling package

--- init.el
+++ init.el
@@ -61,16 +61,18 @@
       use-package-verbose t)
 
 (require 'use-package)
-
-(use-package restart-emacs) ; Restart emacs from within emacs
-
-(use-package atom-one-dark-theme ; Atom one dark theme
+;; Restart emacs from within emacs
+(use-package restart-emacs)
+;; Atom one dark theme
+(use-package atom-one-dark-theme
   :init (load-theme 'atom-one-dark t))
-
 ;; An atom-one-dark theme for smart-mode-line
 (use-package smart-mode-line-atom-one-dark-theme)
-
-(use-package smart-mode-line ; A coded smart mode-line
+;; A coded smart mode-line
+(use-package smart-mode-line
   :init (setq sml/no-confirm-load-theme t
 	      sml/theme 'atom-one-dark)
   (sml/setup))
+;; Make emacs scroll smoothly
+(use-package smooth-scrolling
+  :init (smooth-scrolling-mode 1))
