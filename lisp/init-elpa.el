f59b65c735d6b2ccdaf16298ef0200450d5104f7
Mystical-C <64483945+MysticalDevil@users.noreply.github.com>
2023-10-20 21:03:43 +0800

    feat: Use straight.el as default package manager

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -16,13 +16,28 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
+;; straight.el -- Next-generation, purely functional package manager for the Emacs hacker.
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
 ;; Use use-package to manage extensions
 (setq use-package-always-ensure t ; Global ensure keyword
       use-package-always-defer t ; Global defer load keyword
       use-package-always-demand nil
       use-package-expand-minimally t
-      use-package-verbose t)
+      use-package-verbose t
+      straight-use-package-by-default t) ; Use straight as default package manager
 
-(require 'use-package)
+(straight-use-package 'use-package)
 
 (provide 'init-elpa)
--- lisp/init-package.el
+++ lisp/init-package.el
@@ -1,10 +1,21 @@
 ;; Restart emacs from within emacs
 (use-package restart-emacs)
 
+;; Future-proof your Emacs Lisp customizations!
+(use-package el-patch)
+
 ;; Benchmarks for require and load calls
 (use-package benchmark-init
   :init (benchmark-init/activate)
   :hook (after-init . benchmark-init/deactivate))
+
+;; Tree sitter
+(use-package tree-sitter-langs)
+(use-package tree-sitter
+  :after tree-sitter-langs
+  :config
+  (global-tree-sitter-mode)
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
 
 ;; Use y_n to replace yes_no
 (use-package emacs
