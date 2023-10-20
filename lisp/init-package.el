1faefbb12a29f2f49be0228e16733feb33df93d4
MysticalDevil <jinpeng7981@outlook.com>
2023-10-20 04:57:41 -0700

    fix: wrong config locate

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -6,27 +6,8 @@
   :init (benchmark-init/activate)
   :hook (after-init . benchmark-init/deactivate))
 
-;; Change font on windows to reduce lag
-(use-package emacs
-  :if (display-graphic-p)
-  :config
-  ;; Font settings
-  (if *is-windows*
-      (progn
-	(set-face-attribute 'default nil :font "Microsoft Yahei Mono 9")
-	(dolist (charset '(kana han symbol cjk-misc bopomofo))
-	  (set-fontset-font (frame-parameter nil 'font)
-			    charset (font-spec :family "Microsoft Yahei Mono" :size 12))))
-    (set-face-attribute 'default nil :font "MesloLGS Nerd Font")))
-
 ;; Use y_n to replace yes_no
 (use-package emacs
   :config (defalias 'yes-or-no-p 'y-or-n-p))
-
-;; Show line number
-(use-package emacs
-  :config
-  (setq display-line-numbers-type 'absolute)
-  (global-display-line-numbers-mode 1))
 
 (provide 'init-package)
--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -12,4 +12,23 @@
 (use-package smooth-scrolling
   :init (smooth-scrolling-mode 1))
 
+;; Change font on windows to reduce lag
+(use-package emacs
+  :if (display-graphic-p)
+  :config
+  ;; Font settings
+  (if *is-windows*
+      (progn
+	(set-face-attribute 'default nil :font "Microsoft Yahei Mono 9")
+	(dolist (charset '(kana han symbol cjk-misc bopomofo))
+	  (set-fontset-font (frame-parameter nil 'font)
+			    charset (font-spec :family "Microsoft Yahei Mono" :size 12))))
+    (set-face-attribute 'default nil :font "MesloLGS Nerd Font")))
+
+;; Show line number
+(use-package emacs
+  :config
+  (setq display-line-numbers-type 'absolute)
+  (global-display-line-numbers-mode 1))
+
 (provide 'init-ui)
