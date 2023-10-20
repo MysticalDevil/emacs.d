e784a9dcd26723e28c87422d4ea5ad830427eb34
MysticalDevil <jinpeng7981@outlook.com>
2023-10-20 04:35:26 -0700

    feat: Chang font on Windows to reduce lag

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -1,4 +1,16 @@
 ;; Restart emacs from within emacs
 (use-package restart-emacs)
 
+(use-package emacs
+  :if (display-graphic-p)
+  :config
+  ;; Font settings
+  (if *is-windows*
+      (progn
+	(set-face-attribute 'default nil :font "Microsoft Yahei Mono 9")
+	(dolist (charset '(kana han symbol cjk-misc bopomofo))
+	  (set-fontset-font (frame-parameter nil 'font)
+			    charset (font-spec :family "Microsoft Yahei Mono" :size 12))))
+    (set-face-attribute 'default nil :font "MesloLGS Nerd Font")))
+
 (provide 'init-package)
