21161a395ca7f52c78bdb317c2ab39a2b646bc87
MysticalDevil <jinpeng7981@outlook.com>
2023-10-20 04:50:23 -0700

    Use y_n to replace yes_no

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -1,6 +1,12 @@
 ;; Restart emacs from within emacs
 (use-package restart-emacs)
 
+;; Benchmarks for require and load calls
+(use-package benchmark-init
+  :init (benchmark-init/activate)
+  :hook (after-init . benchmark-init/deactivate))
+
+;; Change font on windows to reduce lag
 (use-package emacs
   :if (display-graphic-p)
   :config
@@ -12,5 +18,9 @@
 	  (set-fontset-font (frame-parameter nil 'font)
 			    charset (font-spec :family "Microsoft Yahei Mono" :size 12))))
     (set-face-attribute 'default nil :font "MesloLGS Nerd Font")))
+
+;; Use y_n to replace yes_no
+(use-package emacs
+  :config (defalias 'yes-or-no-p 'y-or-n-p))
 
 (provide 'init-package)
