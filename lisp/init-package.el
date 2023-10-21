c217ea1f15a320c21d2af81d1c1278c997d95735
Mystical-C <64483945+MysticalDevil@users.noreply.github.com>
2023-10-21 14:19:43 +0800

    feat: Add ivy plugin -- enhanced search, input. Add company plugin -- auto complete. Add flycheck -- grammer check

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -1,3 +1,8 @@
+;;; init-package.el -- settings for packages
+
+;;; Commentary:
+;;; Code:
+
 ;; Restart emacs from within emacs
 (use-package restart-emacs)
 
@@ -43,4 +48,62 @@
   :bind (("<M-up>" . drag-stuff-up)
 	 ("<M-down>" . drag-stuff-down)))
 
+;; Ivy - a generic completion frontend for Emacs, Swiper - isearch with an overview, and more. Oh, man!
+(use-package ivy
+  :defer 1
+  :demand
+  :hook (after-init . ivy-mode)
+  :config
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t
+	ivy-initial-inputs-alist nil
+	ivy-count-format "[%d/%d]"
+	enable-recursive-minibuffers t
+	ivy-re-builders-alist '((t . ivy--regex-ignore-order))))
+
+(use-package counsel
+  :after (ivy)
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x C-f" . counsel-find-file)
+	 ("C-c f" . counsel-recentf)
+	 ("C-c g" . counsel-git)))
+
+(use-package swiper
+  :after ivy
+  :bind (("C-s" . swiper)
+	 ("C-r" . swiper-isearch-forward))
+  :config (setq swiper-action-recenter t
+		swiper-include-line-number-in-search t))
+
+;; Modular in-buffer completion framework for Emacs
+(use-package company
+  :config
+  (setq company-dabbrev-code-everywhere t
+	company-dabbrev-code-modes t
+	company-dabbrev-code-other-buffers 'all
+	company-dabbrev-downcase nil
+	company-dabbrev-ignore-case t
+	company-dabbrev-other-buffers 'all
+	company-require-match nil
+	company-minimum-prefix-length 2
+	company-show-numbers t
+	company-tooltip-limit 20
+	company-idle-delay 0
+	company-echo-dely 0
+	company-tooltip-offset-display 'scrollbar
+	company-begin-commands '(self-insert-command))
+  (push '(company0semantic :with company-yasnippet) company-backends)
+  :hook ((after-init . global-company-mode)))
+
+;; On-the-fly syntax checking
+(use-package flycheck
+  :hook (after-init . global-flycheck-mode))
+					; :hook (prog-mode . flycheck-mode) enable on programing languages
+
+;; Enhanced the undo operate
+(use-package undo-tree
+  :init (global-undo-tree-mode)
+  :custom (undo-tree-auto-save-history nil))
+
 (provide 'init-package)
+;;; init-package.el ends here
