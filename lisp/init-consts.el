f4071b9114f2165eebdf82aab140031cd9fa2a60
Mystical-C <64483945+MysticalDevil@users.noreply.github.com>
2023-10-22 13:16:59 +0800

    Fix a error

--- lisp/init-consts.el
+++ lisp/init-consts.el
@@ -7,7 +7,7 @@
 (defconst *is-linux* (eq system-type 'gnu/linux))
 (defconst *is-windows* (or (eq system-type 'ms-dos) (eq system-type 'windows-nt)))
 
-(defconst *ts-avaiable* (or (>= emacs-major-version 29) (treesit-available-p)))
+(defconst *ts-avaiable* (>= emacs-major-version 29))
 
 (provide 'init-consts)
 ;;; init-consts.el ends here
